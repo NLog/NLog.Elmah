e8cceb6e1a9cb81dca94b5d66ffaebe3462f5616
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-01-04 23:50:33 +0100

    Update AssemblyInfo.cs
    added AssemblyInformationalVersion + update copyright

--- NLog.Elmah/Properties/AssemblyInfo.cs
+++ NLog.Elmah/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NLog")]
 [assembly: AssemblyProduct("NLog.Elmah")]
-[assembly: AssemblyCopyright("Copyright © NLog 2015")]
+[assembly: AssemblyCopyright("Copyright © NLog 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("NLog")]
