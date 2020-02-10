ed49b7d1373e100b50c059b2bbef7d5db2992c46
Michael Möhler <m.moehler@xf9.de>
2020-02-10 06:29:00 +0100

    Check user field

--- NLog.Elmah.Tests/given_target_with_loglevelastype_not_set_and_log_event_with_no_exception_when_logging_info.cs
+++ NLog.Elmah.Tests/given_target_with_loglevelastype_not_set_and_log_event_with_no_exception_when_logging_info.cs
@@ -50,6 +50,13 @@ namespace NLog.Elmah.Tests.ElmahTargetTests
 			Assert.That(error.Source, Is.Empty);
 		}
 
+        [Test]
+        public void should_not_set_user()
+        {
+            var error = ErrorLog.GetFirstError();
+            Assert.That(error.User, Is.Empty);
+        }
+
 		[Test]
 		public void should_set_time_to_now()
 		{
